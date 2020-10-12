99bb7f21bff0ad55c8034c2d6a2894b66463b310
Eric <954512202@qq.com>
2020-10-12 13:09:14 +0800

    cannot save checked status solved

--- Todolist_Project/todolist/src/components/TodoList.vue
+++ Todolist_Project/todolist/src/components/TodoList.vue
@@ -1,99 +1,99 @@
 <template>
-      <el-main>
-        <el-row :gutter="10" style="height:1px">
-          <el-col :span="6"></el-col>
-        </el-row>
-        <el-row style="padding-bottom:10px;">
+  <el-main>
+    <el-row :gutter="10" style="height:1px">
+      <el-col :span="6"></el-col>
+    </el-row>
+    <el-row style="padding-bottom:10px;">
+      <el-col :span="8">
+        <div class="empty"></div>
+      </el-col>
+      <el-col :span="8">
+        <el-input
+          v-model.trim="content"
+          placeholder="请输入内容,enter保存哈"
+          @keyup.enter.native="saveTodo"
+          clearable
+        />
+      </el-col>
+      <el-col :span="8">
+        <div class="empty"></div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8">
+        <div class="empty"></div>
+      </el-col>
+      <el-col :span="3">
+        <el-select v-model="selected" placeholder="请选择" tabindex="0">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <div style="height:10px"></div>
+    <el-row :gutter="0">
+      <el-col :span="8">
+        <div class="empty"></div>
+      </el-col>
+      <el-col :span="3">
+        <div class="block">
+          <el-pagination
+            :small="false"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage"
+            :page-sizes="[5,10,15,20]"
+            :page-size.sync="pageSize"
+          ></el-pagination>
+        </div>
+      </el-col>
+    </el-row>
+    <div style="height:10px"></div>
+    <transition-group name="flip-list">
+      <template v-for="item in changeOption">
+        <el-row :key="item.id">
           <el-col :span="8">
             <div class="empty"></div>
           </el-col>
           <el-col :span="8">
-            <el-input
-              v-model.trim="content"
-              placeholder="请输入内容,enter保存哈"
-              @keyup.enter.native="saveTodo"
-              clearable
-            />
+            <el-col :span="1">
+              <el-checkbox
+                v-model="item.value"
+                @change="updateItem(item.value,item.id,item.content)"
+              ></el-checkbox>
+            </el-col>
+            <el-col :span="10" class="ellipsis">{{item.content}}</el-col>
+            <el-col :span="5">
+              <div>
+                <el-link icon="el-icon-edit" @click="triggerChange(true,item)"></el-link>
+              </div>
+            </el-col>
+            <el-col
+              :span="6"
+              class="color-dark-light"
+              style="font-size:13px"
+            >{{item.created | capitalize}}</el-col>
+            <el-col :span="2">
+              <template>
+                <el-popconfirm title="这是一段内容确定删除吗？" v-on:onConfirm="deleteItem(item.id)">
+                  <el-button icon="el-icon-delete" class="delete_btn" slot="reference"></el-button>
+                </el-popconfirm>
+              </template>
+            </el-col>
           </el-col>
           <el-col :span="8">
             <div class="empty"></div>
           </el-col>
         </el-row>
-        <el-row>
-          <el-col :span="8">
-            <div class="empty"></div>
-          </el-col>
-          <el-col :span="3">
-            <el-select v-model="selected" placeholder="请选择" tabindex="0">
-              <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
-          </el-col>
-        </el-row>
-        <div style="height:10px"></div>
-        <el-row :gutter="0">
-          <el-col :span="8">
-            <div class="empty"></div>
-          </el-col>
-          <el-col :span="3">
-            <div class="block">
-              <el-pagination
-                :small="false"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="total"
-                @current-change="handleCurrentChange"
-                :current-page.sync="currentPage"
-                :page-sizes="[5,10,15,20]"
-                :page-size.sync="pageSize"
-              ></el-pagination>
-            </div>
-          </el-col>
-        </el-row>
-        <div style="height:10px"></div>
-        <transition-group name="flip-list">
-          <template v-for="item in changeOption">
-            <el-row :key="item.id">
-              <el-col :span="8">
-                <div class="empty"></div>
-              </el-col>
-              <el-col :span="8">
-                <el-col :span="1">
-                  <el-checkbox v-model="item.value" @change="changeStatus(item.id,''+item.value+'')"></el-checkbox>
-                </el-col>
-                <el-col :span="10" class="ellipsis">{{item.content}}</el-col>
-                <el-col :span="5">
-                  <div>
-                    <el-link
-                      icon="el-icon-edit"
-                      @click="triggerChange(true,item)"
-                    ></el-link>
-                  </div>
-                </el-col>
-                <el-col
-                  :span="6"
-                  class="color-dark-light"
-                  style="font-size:13px"
-                >{{item.created | capitalize}}</el-col>
-                <el-col :span="2">
-                  <template>
-                    <el-popconfirm title="这是一段内容确定删除吗？" v-on:onConfirm="deleteItem(item.id)">
-                      <el-button icon="el-icon-delete" class="delete_btn" slot="reference"></el-button>
-                    </el-popconfirm>
-                  </template>
-                </el-col>
-              </el-col>
-              <el-col :span="8">
-                <div class="empty"></div>
-              </el-col>
-            </el-row>
-          </template>
-        </transition-group>
-        <v-Drawer ref="d1" @changestatus="changeStatus"></v-Drawer>
-      </el-main>
+      </template>
+    </transition-group>
+    <v-Drawer ref="d1" @changestatus="changeStatus"></v-Drawer>
+  </el-main>
 </template>
 
 <script>
@@ -185,17 +185,21 @@ export default {
     changeStatus (sitem) {
       let value = sitem.item.value
       let id = sitem.item.id
+      let title = sitem.title
+      this.updateItem(value, id, title)
+    },
+    updateItem (value, id, title) {
       for (let item of this.list) {
         if (item.id === id) {
           item.value = value + '' === 'true' ? true : ''
-          if (!sitem.title) {
+          if (!title) {
             this.$message({
               message: '友情提示，内容不能为空哦',
               type: 'warning'
             })
             return
           }
-          item.content = sitem.title
+          item.content = title
         }
       }
       this.saveLocal()
@@ -225,7 +229,6 @@ export default {
 }
 </script>
 <style lang="css" scoped>
-
 .ellipsis {
   /* 显示一行，省略号 */
   white-space: nowrap;
@@ -266,8 +269,7 @@ export default {
   transition: all 1s;
 }
 
-.flip-list-leave-to
-{
+.flip-list-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
